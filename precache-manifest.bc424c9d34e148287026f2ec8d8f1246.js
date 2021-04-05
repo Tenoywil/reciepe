a0676234811c343abd8f73cec7da421cbe1084c1
Tenoywil <tenoy.wilson@gmail.com>
2021-04-05 12:57:21 -0500

    Updates

---
+++ precache-manifest.bc424c9d34e148287026f2ec8d8f1246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d22e68515294f30a5efd23b3070a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0049cbc09a2755ae20",
+    "url": "/static/css/main.3b368e17.chunk.css"
+  },
+  {
+    "revision": "bdab66ccd36b745cf6f3",
+    "url": "/static/js/2.04dcda93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04dcda93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0049cbc09a2755ae20",
+    "url": "/static/js/main.47b8eb9a.chunk.js"
+  },
+  {
+    "revision": "c8dec774f6c7216cf27b",
+    "url": "/static/js/runtime-main.707450e7.js"
+  }
+]);
